4f9d96fd25671194ede491730f94dcc817f3993f
bryan-alvaredo <bryan.alvaredo95@gmail.com>
2021-03-02 00:03:41 -0300

    login, register, logout, etc

--- TrabajoFinal/views.py
+++ TrabajoFinal/views.py
@@ -26,26 +26,30 @@ def logout(request):
     return redirect('/')
 
 def login(request):
-    # Crea el formulario de autenticación vacío
-    form = AuthenticationForm()
-    if request.method == "POST":
-        # Añade los datos recibidos al formulario
-        form = AuthenticationForm(data=request.POST)
-        # Si el formulario es válido...
-        if form.is_valid():
-            # Recupera las credenciales validadas
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            # Verifica las credenciales del usuario
-            user = authenticate(username=username, password=password)
-            # Si existe un usuario con ese nombre y contraseña
-            if user is not None:
-                # Hace el login manualmente
-                do_login(request, user)
-                # Y redirecciona a la portada
-                return redirect('/')
-    # Si llega al final renderiza el formulario
-    return render(request, "login.html", {'form': form})
+    # Si ya está autenticado, redirige a home, sino dirige a login
+    if request.user.is_authenticated:
+        return render(request, "home.html")
+    else:
+        # Crea el formulario de autenticación vacío
+        form = AuthenticationForm()
+        if request.method == "POST":
+            # Añade los datos recibidos al formulario
+            form = AuthenticationForm(data=request.POST)
+            # Si el formulario es válido...
+            if form.is_valid():
+                # Recupera las credenciales validadas
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                # Verifica las credenciales del usuario
+                user = authenticate(username=username, password=password)
+                # Si existe un usuario con ese nombre y contraseña
+                if user is not None:
+                    # Hace el login manualmente
+                    do_login(request, user)
+                    # Y redirecciona a la portada
+                    return redirect('/')
+        # Si llega al final renderiza el formulario
+        return render(request, "login.html", {'form': form})
 
 def register(request):
     # Crea el formulario de autenticación vacío
