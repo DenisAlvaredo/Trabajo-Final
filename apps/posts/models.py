adb1f51f4bf0dab33b355f6351737d6ae9bc14d3
Denis Joaquin Alvaredo Gavilán <denis_alvaredo@hotmail.com>
2021-03-08 17:21:14 -0300

    arreglado que pida autor

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 from apps.users.models import User
 from ckeditor.fields import RichTextField
@@ -12,22 +13,31 @@ class Categoria(models.Model):
         verbose_name = 'Categoría' #los verbose name, es para que se utilice un nombre si solo existe una y el otro si hay mas de una categoria
         verbose_name_plural = 'Categorías'
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
     def __str__(self):
         return self.nombre
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.SET_DEFAULT, default=1)
     titulo = models.CharField('Título del post', max_length = 100, null = False, blank = False)
     categoria = models.ForeignKey('Categoria', on_delete = models.CASCADE)
     contenido = models.TextField('Contenido del post', max_length = 5255, null = False, blank = False)
     #miniatura = models.URLField('URL de la imagén', max_length = 255, null = False, blank = False)
     miniatura = models.ImageField()
-    slug = models.CharField('Slug', max_length = 100, blank = False, null = False)
-    fecha_publicacion = models.DateTimeField('Fecha de publicación', default = timezone.now) 
+    slug = models.SlugField('Slug', max_length = 100, blank = False, null = False)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación', default = timezone.now()) 
     ultima_actualizacion = models.DateTimeField('Última actualización', auto_now = True)
 
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.titulo
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView
 from .models import Post, Categoria
 from .forms import PostForm
 
@@ -14,38 +14,7 @@ class PostCreateView(LoginRequiredMixin, CreateView): #entra heredando el create
     template_name = 'posts/post_create.html'
     success_url = reverse_lazy('home')
 
-############################LISTAR POSTS##################################
-
-class PostListView(ListView):
-    queryset = Post.objects.all().order_by('fecha_creacion')
-    model = Post # Modelo creado en models.py el cual se creo con los campos a usar en la creacion del post
-    template_name = 'posts/post_list'
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
-
-#####################CATEGORIAS DE LOS POSTS##############################
-
-class Cultura(TemplateView): #no olvidarse importar TemplateView
-    template_name = 'posts/cultura.html'
-
-class Tecnologia(TemplateView):
-    template_name = 'posts/tecnologia.html'
-
-class Gaming(TemplateView):
-    template_name = 'posts/gaming.html'
-
-class Peliculas(TemplateView):
-    template_name = 'posts/peliculas.html'
-
-class Series(TemplateView):
-    template_name = 'posts/series.html'
-
-class Anime(TemplateView):
-    template_name = 'posts/anime.html'
-
-class MisPosts(TemplateView):
-    template_name = 'posts/misposts.html'
-
-#####################AQUI TERMINA CATEGORIAS DE LOS POSTS####################
