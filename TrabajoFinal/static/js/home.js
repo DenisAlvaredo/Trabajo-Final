55378b554b0ba8d2c0648c38813a51620e127eb0
bryan-alvaredo <bryan.alvaredo95@gmail.com>
2021-03-30 21:59:49 -0300

    boton ir arriba

--- TrabajoFinal/static/js/home.js
+++ TrabajoFinal/static/js/home.js
@@ -8,14 +8,23 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function(){ //Boton ir arriba
+$(document).ready(function () {
+  //Hacia arriba
   irArriba();
 });
 
-function irArriba(){
-  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+function irArriba() {
+  $(".ir-arriba").click(function () {
+    $("body,html").animate({ scrollTop: "0px" }, 1000);
   });
-  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(600);
+    } else {
+      $(".ir-arriba").slideUp(600);
+    }
+  });
+  $(".ir-abajo").click(function () {
+    $("body,html").animate({ scrollTop: "1000px" }, 1000);
+  });
 }
